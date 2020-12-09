80d43d4286d74b3de4c79926c8927b9738181b47
Jiselle Liu <jiselle03@gmail.com>
2020-12-08 19:47:15 -0800

    Add credentials

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '/api/*',
       headers: :any,
+      credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
