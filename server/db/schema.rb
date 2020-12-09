aace7d09d226e9939642551dfc97d358c2c73b81
Jiselle Liu <jiselle03@gmail.com>
2020-12-09 12:47:56 -0800

    Run migrations for items and stores

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_020739) do
+ActiveRecord::Schema.define(version: 2020_12_09_204449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 8, scale: 2
+    t.boolean "availability"
+    t.text "description"
+    t.string "customization"
+    t.string "category"
+    t.bigint "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_items_on_store_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.string "city"
+    t.string "province"
+    t.string "country"
+    t.string "postal_code"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_020739) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "stores"
 end
