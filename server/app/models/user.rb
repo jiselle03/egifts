9ef1ec5b482120a87f12a20dcb3582cfa216c270
Jiselle Liu <jiselle03@gmail.com>
2020-12-08 18:19:57 -0800

    Add User model

---
+++ server/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    # has_many :roles, dependent: :destroy
+    # has_many :purchased_items, through: :transactions, source: :item
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true,
+    format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    
+    has_secure_password
+end
