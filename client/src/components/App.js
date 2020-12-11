80e1d02c1d68acf250422f432b7b0d9a43632684
Jiselle Liu <jiselle03@gmail.com>
2020-12-10 16:50:07 -0800

    Add global styles and theme

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,63 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import User from '../api/user';
+import Session from '../api/session';
+
+import AuthRoute from './AuthRoute';
+import LoadingCircle from './LoadingCircle';
+import NavBar from './navbar/NavBar';
+
+import WelcomePage from './pages/WelcomePage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+import { ThemeProvider } from 'styled-components';
+import theme from '../global/theme';
+import GlobalStyles from '../global/global';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUser = useCallback(() => {
+    User.current().then(user => {
+      typeof user.id !== "number" ? setCurrentUser(null) : setCurrentUser(user);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const destroySession = () => Session.destroy().then(setCurrentUser(null));
+
+  useEffect(() => getUser(), [getUser]);
+
+  if (isLoading) return (<LoadingCircle />);
+
   return (
-    <div>
-      <header>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyles />
+
+        <NavBar 
+          currentUser={currentUser} 
+          onSignOut={destroySession} 
+        />
+
+        <Switch>
+          <Route exact path='/' component={WelcomePage}/>
+          <Route 
+            path='/sign-in'
+            render={routeProps => <SignInPage {...routeProps} onSignIn={getUser} />}  
+          />
+          <Route 
+            path='/sign-up'
+            render={routeProps => <SignUpPage {...routeProps} onSignUp={getUser} />}  
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
