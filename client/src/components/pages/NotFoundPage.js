f352a253595efd7a0e46f9ae2233b38deb82dfa6
Jiselle Liu <jiselle03@gmail.com>
2020-12-10 16:51:11 -0800

    Set up pages

---
+++ client/src/components/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFoundPage = () => (<h2>404 Not Found</h2>);
+
+export default NotFoundPage;
---
+++ client/src/components/pages/WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomePage = props => {
+    return (
+        <main>
+            
+        </main>
+    );
+};
+
+export default WelcomePage;
