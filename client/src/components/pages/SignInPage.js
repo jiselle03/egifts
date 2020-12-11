f0701a7a2d09cf5d34cfa6dabffe62ae65513ad0
Jiselle Liu <jiselle03@gmail.com>
2020-12-10 23:18:29 -0800

    Add forms in SignInPage and SignUpPage

--- client/src/components/pages/SignInPage.js
+++ client/src/components/pages/SignInPage.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SignInPage = () => {
+import Session from '../../api/session';
+
+import Form from '../form/Form';
+import Input from '../form/Input';
+import Button from '../Button';
+
+const SignInPage = props => {
+    const { onSignIn } = props;
+
+    const [errors, setErrors] = useState([]);
+
+    const createSession = event => {
+        event.preventDefault();
+        const { currentTarget: form } = event;
+        const fd = new FormData(form);
+        setErrors([]);
+        
+        Session.create({
+            email: fd.get("email"),
+            password: fd.get("password")
+        }).then(data => {
+            if (data.status === 404) {
+                setErrors([...errors, { message: "Wrong email or password"}]);
+            } else {
+                if (typeof onSignIn === "function") onSignIn();
+                props.history.push("/");
+            };
+        });
+    };
+
     return (
-        <>
-        </>
+        <main>
+            <Form onSubmit={createSession}>
+                <Input type="email" name="email" placeholder="Email" />
+                <Input type="password" name="password" placeholder="Password" />
+                <Button primary type="submit">Sign In</Button>
+            </Form>
+        </main>
     );
 };
 
--- client/src/components/pages/SignUpPage.js
+++ client/src/components/pages/SignUpPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Form from '../form/Form';
+import Input from '../form/Input';
+
 const SignUpPage = () => {
     return (
-        <>
-        </>
+        <main>
+            <Form>
+                <Input type="text" placeholder="First Name" />
+                <Input type="text" placeholder="Last Name" />
+                <Input type="email" placeholder="Email" />
+                <Input type="tel" placeholder="Phone Number" />
+                <Input type="password" placeholder="Password" />
+                <Input type="password" placeholder="Password Confirmation" />
+            </Form>
+        </main>
     );
 };
 
