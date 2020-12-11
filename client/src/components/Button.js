9cb07d90e27a6d2efbc02eeb5e4bbb0e6290297c
Jiselle Liu <jiselle03@gmail.com>
2020-12-10 23:17:18 -0800

    Add variations to Button component

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 const Button = styled.button`
     border: none;
     border-radius: 5px;
-    background: ${({ theme }) => theme.background};
+    background: ${({ theme, primary }) => primary ? theme.accent : theme.background};
     color: ${({ theme }) => theme.primary};
     padding: 1rem;
     margin: 1rem 0.5rem;
     text-transform: uppercase;
     letter-spacing: 0.1rem;
     cursor: pointer;
+    float: right;
 `;
 
 export default Button;
