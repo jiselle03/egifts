14b36a70179a1c0b44d020338dc933c636fb0051
Jiselle Liu <jiselle03@gmail.com>
2020-12-17 21:10:51 -0800

    Style links

--- client/src/components/grid/Card.js
+++ client/src/components/grid/Card.js
@@ -4,6 +4,11 @@ const Card = styled.div`
     display: flex;
     flex-direction: column;
     border: 1px solid ${({ theme }) => theme.secondary};
+    
+    a {
+        text-decoration: none;
+        color: ${({ theme }) => theme.primary};
+    }
 `;
 
 export default Card;
