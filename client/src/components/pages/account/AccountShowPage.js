6f57dcdda307fbc0b42dc9ec3445a23b7218f853
Jiselle Liu <jiselle03@gmail.com>
2020-12-20 21:16:28 -0800

    Change Table to styled component to allow for use with other props

--- client/src/components/pages/account/AccountShowPage.js
+++ client/src/components/pages/account/AccountShowPage.js
@@ -3,13 +3,26 @@ import React from 'react';
 import Table from '../../table/Table';
 
 const AccountShowPage = props => {
-    const { currentUser } = props;
+    const { first_name, last_name, email, phone_number } = props.currentUser;
 
     return (
         <>
-            Hello, {currentUser.first_name}!
+            Hello, {first_name}!
 
-            <Table currentUser={currentUser} />
+            <Table>
+                <tr>
+                    <th>Name</th>
+                    <th>{first_name}{last_name}</th>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <th>{email}</th>
+                </tr>
+                <tr>
+                    <th>Phone</th>
+                    <th>{phone_number}</th>
+                </tr>
+            </Table>
         </>
     );
 };
--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,30 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const Table = props => {
-    const { first_name, last_name, email, phone_number } = props.currentUser;
-
-    return (
-        <StyledTable>
-            <tr>
-                <th>Name</th>
-                <th>{first_name}{last_name}</th>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <th>{email}</th>
-            </tr>
-            <tr>
-                <th>Phone</th>
-                <th>{phone_number}</th>
-            </tr>
-        </StyledTable>
-    );
-};
+const Table = styled.table`
+width: 100%;
+border: 1px solid #f3f3f3;
+`;
 
 export default Table;
-
-const StyledTable = styled.table`
-    width: 100%;
-    border: 1px solid #f3f3f3;
-`;
