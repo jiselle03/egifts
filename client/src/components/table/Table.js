f050812d521837d5797d29cd265c321ddd60e3cd
Jiselle Liu <jiselle03@gmail.com>
2020-12-21 12:39:17 -0800

    Add margin

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 const Table = styled.table`
-width: 100%;
-border: 1px solid #f3f3f3;
+    width: 100%;
+    border: 1px solid #f3f3f3;
+    margin: 2rem;
 `;
 
 export default Table;
