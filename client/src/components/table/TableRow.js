3ee2bb00fedc35310b8ef49ee1a7ca4545b74124
Jiselle Liu <jiselle03@gmail.com>
2020-12-21 12:38:09 -0800

    Style table cells

--- client/src/components/table/TableRow.js
+++ client/src/components/table/TableRow.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const TableRow = props => {
     const { name, content } = props;
 
     return (
         <tr>
-            <th>{name}</th>
-            <th>{content}</th>
+            <TableCell weight="600">{name}</TableCell>
+            <TableCell weight="400">{content}</TableCell>
         </tr>
     );
 };
 
 export default TableRow;
+
+const TableCell = styled.th`
+    font-weight: ${({ weight }) => weight};
+`;
