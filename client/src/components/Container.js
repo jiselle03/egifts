37f1c821fafe99ac5d53b1551f91360392bc8171
Jiselle Liu <jiselle03@gmail.com>
2020-12-21 12:53:29 -0800

    Fix margins

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { bool, object } from 'prop-types';
 
 const Container = styled.main`
-    width: calc(100% - ${({ theme, open }) => open ? theme.drawer : "0"});
+    width: calc(100% - ${({ theme, open }) => open ? theme.drawer : "2rem"} - 2rem);
     margin-top: 5rem;
-    margin-left: ${({ theme, open }) => open ? theme.drawer : "2rem"};
+    margin-left: calc(${({ theme, open }) => open ? theme.drawer : "0rem"} + 2rem);
 `;
 
 Container.propTypes = {
