05fc76d9911a0d564158c5b73e4df0b5bfcfd66c
Jiselle Liu <jiselle03@gmail.com>
2020-12-22 17:11:06 -0800

    Transform headings to uppercase

--- client/src/global/global.js
+++ client/src/global/global.js
@@ -20,12 +20,20 @@ const GlobalStyles = createGlobalStyle`
     text-rendering: optimizeLegibility;
   }
 
+  h2, h3, h4, h5, h6 {
+    text-transform: uppercase;
+  }
+
   h1 {
     font-size: 3rem;
   }
 
   h2 {
     font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 2rem;
   }
   `;
 
