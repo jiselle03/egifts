99ca1ba8533eb12e0dd093132188ce9d79485aec
Jiselle Liu <jiselle03@gmail.com>
2020-12-24 11:58:38 -0800

    Add store items to StoreShowPage

--- client/src/components/pages/StoreShowPage.js
+++ client/src/components/pages/StoreShowPage.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import Store from '../../api/store';
 
 import { Text } from '../Typography';
+import { Grid, Card } from '../Grid';
 
 const StoreShowPage = props => {
     const [store, setStore] = useState({});
 
     useEffect(() => {
         Store.one(props.match.params.id).then(store => setStore(store));
-    }, []);
+    }, [props.match.params.id]);
 
     return (
-        <Text>{store.name}</Text>
+        <>
+            <Text>{store.name}</Text>
+            <Grid>
+                {store && store.items && store.items.map(item => 
+                    <Card key={item.id}>
+                        <Link to={`/stores/${store.id}/items/${item.id}`}>{item.name}</Link>
+                    </Card>
+                )}
+            </Grid>
+        </>
     );
 };
 
