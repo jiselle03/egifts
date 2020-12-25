44ee643732d0fe6fcded51fe0c392c9354d70864
Jiselle Liu <jiselle03@gmail.com>
2020-12-25 15:42:50 -0800

    Fix mobile scroll when Menu is open

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -4,8 +4,10 @@ import { bool, object } from 'prop-types';
 const Container = styled.main`
     margin: 5rem ${({ open }) => open ? "0rem" : "2rem"};
     width: calc(${({ open }) => open ? "0vw" : "100vw"} - 4rem);
+    display: ${({ open }) => open ? "none" : "block"};
 
     @media screen and (min-width: ${({ theme }) => theme.medium}) {
+        display: block;
         width: calc(100% - ${({ theme, open }) => open ? theme.drawer : "2rem"} - 2rem);
         margin-left: calc(${({ theme, open }) => open ? theme.drawer : "0rem"} + 2rem);
     }
