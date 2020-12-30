8a75b8ab83f0e625710f93ca76bef397cbd066f4
Jiselle Liu <jiselle03@gmail.com>
2020-12-29 20:39:22 -0800

    Add edit button for profile

--- client/src/components/pages/account/AccountShowPage.js
+++ client/src/components/pages/account/AccountShowPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Table, TableRow } from '../../Table';
-import { Title } from '../../Typography';
+import { Title, Text } from '../../Typography';
+import Flex from '../../Flex';
 
 const AccountShowPage = props => {
     const { first_name, last_name, email, phone_number } = props.currentUser;
@@ -10,7 +12,11 @@ const AccountShowPage = props => {
         <>
             <Title as="h4">Hello, {first_name}!</Title>
 
-            <Title as="h3">Profile</Title>
+            <Flex>
+                <Title as="h3">Profile</Title>
+                <Link to="/account/edit">Edit</Link>
+            </Flex>
+            
             <Table>
                 <tbody>
                     <TableRow name="Name" content={`${first_name} ${last_name}`}/>
