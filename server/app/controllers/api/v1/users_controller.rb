54e96442305e322b068eb70ae63ecf6d3dfb7e5f
Jiselle Liu <jiselle03@gmail.com>
2021-01-01 17:45:50 -0800

    Add methods for user update and update_password

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
     before_action :authenticate_user!, except: [:create]
+    before_action :find_user, only: [:update, :update_password]
+    before_action :authorize!, only: [:update]
 
     def current
         render json: current_user
@@ -22,16 +24,79 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    def update
+        if @user.update user_params
+            render json: { 
+                id: @user.id 
+            }
+        else
+            render(
+                json: { 
+                    errors: @user.errors 
+                },
+                status: 422
+            )
+        end
+    end
+
+    def update_password
+        if @user&.authenticate params[:user][:current_password]
+            new_password = params[:user][:new_password]
+            new_password_confirmation = params[:user][:new_password_confirmation]
+            new_password_different = new_password != params[:user][:current_password]
+            password_confirmed = new_password == new_password_confirmation
+
+            if new_password_different && password_confirmed
+                if @user.update password: new_password, password_confirmation: new_password_confirmation
+                    render json: { 
+                        id: @user.id,
+                        message: "Your password has been successfully updated."
+                    }
+                else 
+                    json: { 
+                        message: "Password update failed. Please try again."
+                    },
+                    status: 422
+                end
+            else
+                json: { 
+                    message: "Your password and password confirmation must match." 
+                },
+                status: 422
+            end
+        else
+            json: {
+                message: "Your current password does not match our records."
+            },
+            status: 422
+        end
+    end
+
     private
+
+    def find_user
+        @user = User.find params[:id]
+    end
 
     def user_params
         params.require(:user).permit(
-        :first_name, 
-        :last_name,
-        :email,
-        :phone_number,
-        :password,
-        :password_confirmation
+            :first_name, 
+            :last_name,
+            :email,
+            :phone_number,
+            :password,
+            :password_confirmation
         )
+    end
+
+    def authorize!
+        unless can?(:crud, @user)
+            render(
+                json: { 
+                    status: 401 
+                },
+                status: 401
+            )
+        end
     end
 end
