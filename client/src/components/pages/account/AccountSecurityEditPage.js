1d229276c775c13419ca6e2bad1bc4b6828dc89b
Jiselle Liu <jiselle03@gmail.com>
2021-01-06 23:31:21 -0800

    Add form for AccountSecurityEditPage

--- client/src/components/pages/account/AccountSecurityEditPage.js
+++ client/src/components/pages/account/AccountSecurityEditPage.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 const AccountSecurityEditPage = props => {
+    const { onUpdateUser, history } = props;
+    const { id, first_name, last_name, email, phone_number } = props.currentUser;
+
+    const updateUser = event => {
+        event.preventDefault();
+        const { currentTarget: form } = event;
+        const fd = new FormData(form);
+
+        const updatedUser = {
+            user: {
+                current_password: fd.get("current_password"),
+                new_password: fd.get("new_password"),
+                new_password_confirmation: fd.get("new_password_confirmation")
+            }
+        };
+    
+        User.update(id, updatedUser)
+          .then(() => {
+            if (typeof onUpdateUser === "function") onUpdateUser();
+            history.push("/account");
+          });
+    };
+
     return (
         <>
+            <Title as="h3">Update Password</Title>
+
+            <Form onSubmit={updateUser}>
+                <Label htmlFor="current_password">Current Password</Label>
+                <Input type="password" name="current_password" />
+
+                <Label htmlFor="new_password">New Password</Label>
+                <Input type="new_password" name="new_password" />
+
+                <Label htmlFor="new_password_confirmation">New Password Confirmation</Label>
+                <Input type="new_password_confirmation" name="new_password_confirmation" />
+
+                <Button primary type="submit">Update</Button>
+            </Form>
         </>
     );
 };
